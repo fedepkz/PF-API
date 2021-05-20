34df031ef071721ca3af4f36046c5d1b6a6f2773
unknown <pablo.hinojosa@ort.edu.ar>
2021-05-19 22:25:24 -0300

    crud contra mongodb

---
+++ data/connection.js
@@ -0,0 +1,26 @@
+const mongoclient = require('mongodb').MongoClient;
+// TODO utilizar varibales de entorno
+const uri = "mongodb+srv://admin:admin123@cluster0.zaghe.mongodb.net/sample_tp2?retryWrites=true&w=majority";
+
+const client = new mongoclient(uri);
+
+let instance = null;
+
+async function getConnection(){
+    if(instance == null){
+        try {
+            instance = await client.connect();
+        } catch (err) {
+            console.log(err.message);
+            throw new Error('problemas al conectarse con mongo');
+        }
+    }
+    return instance;
+}
+
+module.exports = {getConnection};
+
+
+
+
+
--- routes/inventors.js
+++ routes/inventors.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const dataInventor = require('../data/inventor');
+const dataInventor = require('../data/inventordb');
+const joi = require('joi');
 
 // /api/inventors/
 router.get('/', async function(req, res, next) {
@@ -17,19 +18,38 @@ router.get('/:id', async (req,res)=>{
     }
 });
 
-router.post('/', async (req, res)=>{
-    //TODO: Validacion
-    let inventor = req.body;
-    inventor = await dataInventor.addInventor(inventor);
-    res.json(inventor);
+router.post('/', async (req, res)=>{    
+    const schema = joi.object({
+        first: joi.string().alphanum().min(3).required(),
+        last: joi.string().alphanum().min(3).required(),
+        year: joi.number().min(1900).max(2020).required()
+    });
+    const result = schema.validate(req.body);
+    console.log(result);
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+    } else{
+        let inventor = req.body;
+        inventor = await dataInventor.addInventor(inventor);
+        res.json(inventor);
+    }    
 });
 
-router.put('/:id', async (req, res)=>{
-    //TODO: validacion
-    let inventor = req.body;
-    inventor._id = req.params.id;
-    dataInventor.updateInventor(inventor);
-    res.json(inventor);
+router.put('/:id', async (req, res)=>{    
+    const schema = joi.object({
+        first: joi.string().alphanum().min(3),
+        last: joi.string().alphanum().min(3),
+        year: joi.number().min(1400).max(2020)
+    });
+    const result = schema.validate(req.body);
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+    } else{
+        let inventor = req.body;
+        inventor._id = req.params.id;
+        dataInventor.updateInventor(inventor);
+        res.json(inventor);
+    }
 });
 
 router.delete('/:id', async (req, res)=>{
